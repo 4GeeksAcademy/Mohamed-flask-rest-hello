99b59fed50c6888e101a1a47a17f2c1dcde31a10
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-06-06 22:45:15 +0000

    creating classes

--- src/models.py
+++ src/models.py
@@ -6,9 +6,36 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[int] = mapped_column(String(100), nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+    # One-to-many: user can create many posts
+    posts: Mapped[list["Post"]] = relationship(back_populates="author", cascade="all, delete")
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String(200), nullable= False)
+    image_url: Mapped[str] = mapped_column(String(20), nullable=False)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('users.id'), nullable=False)
+
+class Like(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.Foreignkey('user.id'),nullable=False)
+    post_id: Mapped[int] = mapped_column(db.Foreignkey('post_id'),nullable=False)
+    
+
+
+
+
+
+
+
+
+
+    
 
 
     def serialize(self):
@@ -17,3 +44,5 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
