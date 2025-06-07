be6dae9d87b816918e3f52dca2f122077e5fd207
Mohamed Lemine tedjih <59097385+Med-lemin@users.noreply.github.com>
2025-06-07 00:39:42 +0000

    update models 1.0.0

--- src/models.py
+++ src/models.py
@@ -5,61 +5,47 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    user_name: Mapped[int] = mapped_column(String(100), unique=True, nullable=False)
+    user_name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
     # One-to-many relationships
-    Posts = db.relationship('Post', backref='user')
-    Likes = db.relationship('Like', backref='user')
-    Comments = db.relationship('Comment', backref='user')
-    Stories = db.relationship('Story', backref='user')
-
-
-class Post(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    content: Mapped[str] = mapped_column(String(200), nullable= False)
-    image_url: Mapped[str] = mapped_column(String(20), nullable=False)
-    user_id: Mapped[int] = mapped_column(db.ForeignKey('users.id'), nullable=False)
-    
-
-class Like(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(db.Foreignkey('user.id'),nullable=False)
-    post_id: Mapped[int] = mapped_column(db.Foreignkey('post_id'),nullable=False)
-
-class Comment(db.Model):
-    id: Mapped[int] = mapped_column(primary_key= True)
-    text: Mapped[str] = mapped_column(String(200), nullable=False)
-    user_id: Mapped[int] = mapped_column(db.Foreignkey('user.id'),nullable=False)
-    post_id: Mapped[int] = mapped_column(db.Foreignkey('post_id'),nullable=False)
-
-class Story(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(db.foreignkey('user_id',nullabld= False))
-
-    
-
-
-
-
-
-
-
-
-
-
-
-    
-
+    posts = db.relationship('Post', backref='user')
+    likes = db.relationship('Like', backref='user')
+    comments = db.relationship('Comment', backref='user')
+    stories = db.relationship('Story', backref='user')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            # do not serialize the password, it's a security breach
         }
-    
 
+class Post(db.Model):
+    __tablename__ = 'post'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    content: Mapped[str] = mapped_column(String(200), nullable=False)
+    image_url: Mapped[str] = mapped_column(String(200), nullable=False)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
+
+class Like(db.Model):
+    __tablename__ = 'like'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(db.ForeignKey('post.id'), nullable=False)
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    text: Mapped[str] = mapped_column(String(200), nullable=False)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
+    post_id: Mapped[int] = mapped_column(db.ForeignKey('post.id'), nullable=False)
+
+class Story(db.Model):
+    __tablename__ = 'story'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(db.ForeignKey('user.id'), nullable=False)
